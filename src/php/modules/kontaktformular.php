657968ce0f6bc200b3d3df8ee05c797e70717deb
roydigerhund <github@matzemachtdesign.de>
2018-05-28 16:35:07 +0200

    Contact Form

---
+++ src/php/modules/kontaktformular.php
@@ -0,0 +1,34 @@
+<div class="kontaktformular">
+
+  <div class="content">
+    
+    <div class="left">
+      
+      <h3>Telefon</h3>
+      <h2><a class="phone" href="tel+49 (0)7520 94499-70" title="Anrufen">+49 (0)7520 94499-70</a></h2>
+      
+      <h3>E-Mail</h3>
+      <h2><a class="email" href="mailto:info@blei-immobilien.de" title="E-Mail schreiben">info@blei-immobilien.de</a></h2>
+      
+      <h3 class="address">Anschrift</h3>
+      <h2>
+        <span>Blei Immobilien & Verwaltung</span><br/>
+        Spiesberg 21/8<br/>
+        88279 Amtzell
+      </h2>
+      
+    </div>
+    
+    <div class="right">
+      
+      <h3>kontaktformular</h3>
+      
+      <h2>Schreiben Sie uns!</h2>
+      
+      <?php echo do_shortcode(get_sub_field('formular')) ?>
+      
+    </div>
+    
+  </div>
+
+</div>
