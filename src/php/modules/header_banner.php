e66d2d05824f9cf89aa0692aec55e213961cc157
BONSEI <david@davids-imac.speedport.ip>
2018-05-30 14:18:55 +0200

    180530_David

--- src/php/modules/header_banner.php
+++ src/php/modules/header_banner.php
@@ -1,29 +1,29 @@
 <div class="header_banner <?php if(!get_sub_field('bild')) echo ' noImage' ?>">
-  
+
   <div class="image <?php the_sub_field('hoehe'); ?>" style="background-image: url('<?php the_sub_field('bild'); ?>')"></div>
-  
+
   <div class="contentWrapper">
-    
+
     <div class="content">
-      
-      <div class="overline">
+
+      <div class="overline <?php the_sub_field('headline_farbe'); ?>">
         <?php the_sub_field('overline'); ?>
       </div>
-      
+
       <div class="headline <?php the_sub_field('headline_farbe'); ?>">
         <?php the_sub_field('headline'); ?>
       </div>
-      
+
       <?php if (($link = get_sub_field('link')) && get_sub_field('hoehe') == 'gross'): ?>
-        <a class="link" href="<?php echo $link['url']; ?>" title="<?php echo $link['title']; ?>" target="<?php echo $link['target']; ?>">
+        <a class="link <?php the_sub_field('headline_farbe'); ?>" href="<?php echo $link['url']; ?>" title="<?php echo $link['title']; ?>" target="<?php echo $link['target']; ?>">
           <?php echo $link['title']; ?>
         </a>
       <?php endif; ?>
-      
+
     </div>
-    
+
     <?php display_breadcrumb(); ?>
-    
+
   </div>
-  
+
 </div>
